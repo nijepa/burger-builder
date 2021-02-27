ff1a28ef669baa0a341670dd5ffa055a5f0def6a
Nikola Pavicevic <nijepa@gmail.com>
2021-02-27 01:23:21 +0100

    Build controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,25 @@ import React from 'react';
 import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-const Burger = () => {
+const Burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + 1} type={igKey} />
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
+    
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please add ingredients</p>
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   )
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -8,14 +8,15 @@ class BurgerIngredient extends Component  {
     let ingredient = null;
 
     switch ( this.props.type ) {
-      case('breed-bottom'):
+      case('bread-bottom'):
         ingredient = <div className={classes.BreadBottom}></div>;
         break;
       case('bread-top'):
-        ingredient = <div className={classes.BreadTop}>
-          <div className={classes.Seeds1}></div>
-          <div className={classes.Seeds2}></div>
-        </div>;
+        ingredient = 
+          <div className={classes.BreadTop}>
+            <div className={classes.Seeds1}></div>
+            <div className={classes.Seeds2}></div>
+          </div>;
         break;
       case('meat'):
         ingredient = <div className={classes.Meat}></div>;
